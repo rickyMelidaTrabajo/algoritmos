ec89119f0965467c0d7772fae996115ff83c03d2
RickyMelidaTrabajo <ricardomelida92.trabajo@gmail.com>
2021-05-31 21:36:22 -0400

    Modify code javascript and python binary search
    Its run the binary search algorithms

--- Ejemplos con JS/binary_search.js
+++ Ejemplos con JS/binary_search.js
@@ -1,10 +1,8 @@
-/* Returns either the index of the location in the array,
-  or -1 if the array did not contain the targetValue */
 var doSearch = function(array, targetValue) {
 	var min = 0;
 	var max = array.length - 1;
     	 
-    while(min < max) {
+    while(min <= max) {
         let mid = Math.floor((max + min)/2); // this is position
         let guess = array[mid]; //this is the number
 
@@ -22,9 +20,8 @@ var doSearch = function(array, targetValue) {
 	
 };
 
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
-		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-var result = doSearch(primes, 73);
+var result = doSearch(primes, 47);
 console.log("Found prime at index " + result);
 
--- Ejemplos con Python/binary_search.py
+++ Ejemplos con Python/binary_search.py
@@ -2,38 +2,32 @@
 def binary_search(arr, item):
     low = 0
     high = len(arr) - 1
-    # Check base case
-    if high >= low:
+    
+    while high >= low:
  
         mid = (high + low) // 2
- 
-        # If element is present at the middle itself
+        
         if arr[mid] == item:
             return mid
  
-        # If element is smaller than mid, then it can only
-        # be present in left subarray
-        elif arr[mid] > x:
+        elif arr[mid] > item:
             high = mid - 1
  
-        # Else the element can only be present in right subarray
         else:
             low = mid + 1
  
-    else:
-        # Element is not present in the array
-        return -1
+    
+    return -1
  
 # Test array
 arr = [ 2, 3, 4, 10, 40 ]
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
-		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
 x = 10
  
 # Function call
 # result = binary_search(arr, 0, len(arr)-1, x)
-result = binary_search(primes, 73)
+result = binary_search(primes, 47)
 
 if result != -1:
     print("Element is present at index", str(result))
